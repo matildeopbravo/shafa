07a020add6a69c4b8c221892c585acf0b9cf1a02
Matide Bravo <matildeopbravo@gmail.com>
2020-12-24 16:16:43 +0000

    Recreated SFTUple, created readblock function, makecode and matrix optimization

--- module_c.c
+++ module_c.c
@@ -1,14 +1,14 @@
-#include "module_c.h"
-/* #include "fsize.h" */
+G#include "module_c.h"
+
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int count_numbers(char* c, FILE* fp_cod) {
     char tmp = *c;
-    char buffer[20];  // to be determined se usamos array dinamico
+    char buffer[20];
     int i = 0;
-
     do {
         buffer[i] = tmp;
         i++;
@@ -18,25 +18,65 @@ int count_numbers(char* c, FILE* fp_cod) {
     *c = tmp;
     return atoi(buffer);
 }
+// uint8_t str_to_num(const char* str, int start, int end) {}
+uint8_t* make_code(const char* str, size_t size, size_t CODE_MAX_SIZE) {
 
-size_t strToNum(const char* str, size_t size) {
-    size_t num = 0;
-    for (int i = size - 1; str[i]; i--) {
-        if (str[i] == '1') {
-            num += str[i] << (size - 1 - i);
+    uint8_t num = 0;
+    uint8_t* arr = calloc(CODE_MAX_SIZE, sizeof(uint8_t));
+    int entire_bytes = size / 8;
+    int byte = 0;
+    size_t i = 0;
+
+    for (; byte < entire_bytes; byte++) {
+
+        num = 0;
+        for (int j = 7; j >= 0; i++, j--) {
+            if (str[i] == '1') {
+                num += 1 << j;  // num = num + 2^j
+            }
         }
+        arr[byte] = num;
     }
-    return num;
+    if (size % 8 != 0) {
+        for (int j = 7; i < size; i++, j--) {
+            if (str[i] == '1') {
+                num += 1 << j;  // num = num + 2^j
+            }
+        }
+        arr[byte] = num;
+    }
+    return arr;
+}
+
+const Piece* get_piece(
+    Piece* matrix,
+    uint8_t offset,
+    uint8_t symbol_index,
+    uint8_t number_symbols) {
+    return (matrix + number_symbols * offset + symbol_index);
+}
+
+void matrix_optimization(Block* block) {
+    // ( ( x - 1 ) | ( m - 1 ) ) + 1 multiple above current
+    // + 8
+    size_t CODE_MAX_SIZE = (((block->biggest_code_size - 1) | 7) + 9) / 8;
+    printf("Code will have %zu bytes\n", CODE_MAX_SIZE);
+    Piece* matrix = malloc(sizeof(Piece) * block->number_symbols * 8);
+    for (int i = 0; i < block->number_symbols; i++) {
+        //uint8_t* code = make_code(sf_code, size, CODE_MAX_SIZE);
+        // add_to_matrix function
+        // matrix_optimization per block
+    }
 }
 
 void print_dictionary(FullSequence* full_seq) {
-
     for (size_t i = 0; i < full_seq->number_blocks; i++) {
-        printf(
-            "Bloco : %zu -> Size : %zu\n", i, full_seq->blocks[i].block_size);
-        for (int x = 0; x < DIC_SIZE; x++) {
-            if (full_seq->blocks[i].symbol_dictionary[x]) {
-                printf("<%s>", full_seq->blocks[i].symbol_dictionary[x]);
+        printf("Bloco: %zu -> Size : %zu\n", i, full_seq->blocks[i].block_size);
+        printf("Max code is : %zu\n", full_seq->blocks[i].biggest_code_size);
+        for (int x = 0; x < DICT_SIZE; x++) {
+            if (full_seq->blocks[i].symbol_dictionary[x].sf_code) {
+                printf(
+                    "<%s>", full_seq->blocks[i].symbol_dictionary[x].sf_code);
             }
             else {
                 putchar('.');
@@ -46,13 +86,38 @@ void print_dictionary(FullSequence* full_seq) {
     }
 }
 
+void read_block(FILE* fp_cod, FullSequence* full_seq, char* c, int nblock) {
+    int index = 0;
+    int max_size = 0;
+
+    for (int ascii = 0; *c != '@';) {
+        if (*c != ';') {
+            char* sequence = malloc(sizeof(char) * 10);
+            int x = 0;
+            for (; *c != ';'; x++) {
+                sequence[x] = *c;
+                *c = fgetc(fp_cod);
+            }
+            sequence[x] = '\0';
+            if (x > max_size) {
+                max_size = x;
+            }
+            full_seq->blocks[nblock].symbol_dictionary[ascii] =
+                (SFTuple){.sf_code = sequence, .index = index++};
+        }
+        else {
+            *c = fgetc(fp_cod);
+            ascii++;
+        }
+    }
+    full_seq->blocks[nblock].biggest_code_size = max_size;
+}
+
 int read_cod(char* cod_file, FullSequence* full_seq) {
-    char* sequence;
     enum parameters param = START;
-    int nblock = 0, error = 1, not_finished = 1;
+    int nblock = 0, not_finished = 1;
     size_t size = 0;
-    // este valor será alterado para 0 quando encontrarmos
-    // @0 e o ciclo para
+    int error = 1;
 
     FILE* fp_cod = fopen(cod_file, "r");
     char c = fgetc(fp_cod);
@@ -65,20 +130,11 @@ int read_cod(char* cod_file, FullSequence* full_seq) {
                     full_seq->compression_type = fgetc(fp_cod);
                     c = fgetc(fp_cod);
                     break;
-
                 case 1:
                     c = fgetc(fp_cod);
                     full_seq->number_blocks = count_numbers(&c, fp_cod);
-                    //                     // O c já está igual ao primeiro
-                    //                     digito
-                    //                     // a count numbers vai deixar o c
-                    //                     = @ e é para onde o file
-                    //                     // pointer aponta e vai devolver
-                    //                     o valor correspondente ao
-                    //                     // numero de blocos (já em long)
                     param = BLOCK_SIZE;
                     break;
-
                 default:
                     c = fgetc(fp_cod);
                     not_finished = c - '0';
@@ -95,26 +151,8 @@ int read_cod(char* cod_file, FullSequence* full_seq) {
                             param = SEQUENCE;
                         }
                         else {  // param == sequence
-                            for (int ascii = 0; c != '@';) {
-                                if (c != ';') {
-                                    sequence = malloc(sizeof(char) * 5);
-                                    int x = 0;
-                                    for (; c != ';'; x++) {
-                                        sequence[x] = c;
-                                        c = fgetc(fp_cod);
-                                    }
-                                    sequence[x] = '\0';
-                                    full_seq->blocks[nblock]
-                                        .symbol_dictionary[ascii] = sequence;
-                                }
-                                else {
-
-                                    c = fgetc(fp_cod);
-                                    ascii++;
-                                }
-                            }
+                            read_block(fp_cod,full_seq,&c,nblock++);
                             param = BLOCK_SIZE;
-                            nblock++;
                         }
                     }
                     break;
@@ -125,22 +163,19 @@ int read_cod(char* cod_file, FullSequence* full_seq) {
 }
 
 int main() {
-    // input file, in this example it's rle but could be just txt
     // char const* symbol_file = "aaa.txt";
-    // char* cod_file = malloc(strlen(symbol_file) + 5);  // (".cod" ++
-    // '\0')
-    FullSequence* my_sequence = calloc(sizeof(FullSequence), 1);
+    // char* cod_file = malloc(strlen(symbol_file) + 5);  // (".cod" ++ '\0')
     // strcpy(cod_file, symbol_file);
     // strcat(cod_file, ".cod");  // codfile = "input.txt.rle.cod"
-    read_cod("bbb.txt.cod", my_sequence);
+    FullSequence* my_sequence = calloc(sizeof(FullSequence), 1);
+    read_cod("aaa.txt.rle.cod", my_sequence);
     print_dictionary(my_sequence);
 
     for (size_t i = 0; i < my_sequence->number_blocks; i++) {
-        for (int j = 0; j < 256; j++) {
-            free(my_sequence->blocks[i].symbol_dictionary[j]);
+        for (int j = 0; j < DICT_SIZE; j++) {
+            free(my_sequence->blocks[i].symbol_dictionary[j].sf_code);
         }
     }
     free(my_sequence);
-
     return 0;
 }
--- module_c.h
+++ module_c.h
@@ -1,31 +1,39 @@
-//#include <stdbool.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define uint8_max 255
-#define MAX_BLOCK_SIZE 67108864      // 64MB
-#define MAX_NUMBER_BLOCKS 5 // 2^32
-#define DIC_SIZE 256
+#define MAX_BLOCK_SIZE 67108864  // 64MB
+#define MAX_NUMBER_BLOCKS 5      // 2^32
+#define DICT_SIZE 256
 
-// not sure about data types yet (eg: long long) and the value of maxblock etc
 enum parameters { START, BLOCK_SIZE, SEQUENCE };
 enum rle_compression { NOT_RLE_COMPRESSED = 'N', RLE_COMPRESSED = 'R' };
 
-// typedef struct {
-//  bool is_garbage;
-//  char * sf_code; // codigo shannon-fano atribuido ao carater nesse bloco
-//} SFTuple;
+typedef struct {
+    char* sf_code;  // codigo shannon-fano atribuido ao carater nesse bloco
+    uint8_t index;  // index atribuido ao simbolo na em cada "peca" do offset
+} SFTuple;
 
 typedef struct block {
-  size_t block_size;
-  char *symbol_dictionary[DIC_SIZE]; // O array tem comprimento 256 (ascii
-                                     // extended?)
+    size_t block_size;
+    uint8_t number_symbols;
+    size_t biggest_code_size;
+    SFTuple symbol_dictionary[DICT_SIZE];  // O array tem comprimento 256 (ascii
+                                           // extended?)
 } Block;
 
-typedef struct {
-  size_t number_blocks;
-  size_t size_first_block;
-  size_t size_last_block;
-  enum rle_compression compression_type;
-  Block blocks[MAX_NUMBER_BLOCKS];
+typedef struct sequence {
+    size_t number_blocks;
+    size_t size_first_block;
+    size_t size_last_block;
+    enum rle_compression compression_type;
+    Block blocks[MAX_NUMBER_BLOCKS];
 } FullSequence;
+
+// Otimização Matriz
+typedef struct piece {
+    uint8_t* code;
+    size_t next;             // Next bit index * Nsimbolos
+    size_t index_next_byte;  // Index do byte até ao qual se faz shift
+} Piece;
